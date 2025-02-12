4c8be6bfef5731e7c55a105998bba95b834abd1e
p0tatoes <elojamp@gmail.com>
2025-02-13 01:19:01 +0800

    added initial models for resources

--- pokepedia/models.py
+++ pokepedia/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Pokemon(models.Model):
+    image = models.URLField()
+    name = models.CharField(max_length=100)
+    types = models.ManyToManyField("pokepedia.Type")
+    height = models.FloatField()
+    weight = models.FloatField()
+    flavor_text = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    weaknesses = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+    )
+
+    def __str__(self):
+        return self.name
